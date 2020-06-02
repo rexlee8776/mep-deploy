d2c862703b6ba984c23cb575f92e0baa9e694a54
jananib <janani.b@huawei.com>
2020-06-02 20:01:15 +0530

    Enable SSL for postgres-db
    Postgrel ssl
    
    Issue-ID: SEC-125
    Signed-off-by: jananib <janani.b@huawei.com>

--- mepauth_deploy.sh
+++ mepauth_deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# clean docker and certs
+docker rm -f mepauth
+# initial variable
+export CertName=mepserver
+CertDir=/tmp/${CertName}
+
+chmod o+r ${CertDir}/*
+chmod og-rwx ${CertDir}/ca.crt
 
 docker run -itd --name mepauth -p 30080:8080 -p 10443:10443\
              --network mep-net \
@@ -6,6 +14,7 @@ docker run -itd --name mepauth -p 30080:8080 -p 10443:10443\
              --link kong-service:kong-service \
              -v /tmp/publickey:/usr/mepauth/publickey \
              -v /tmp/privatekey:/usr/mepauth/privatekey \
+             -v /tmp/mepserver/ca.crt:/usr/mepauth/conf/ca.crt \
              -e "MEPAUTH_DB_NAME=kong" \
              -e "MEPAUTH_DB_USER=kong" \
              -e "MEPAUTH_DB_PASSWD=kong" \
@@ -13,6 +22,6 @@ docker run -itd --name mepauth -p 30080:8080 -p 10443:10443\
              -e "MEPAUTH_DB_PORT=5432" \
              -e "MEPAUTH_APIGW_HOST=kong-service" \
              -e "MEPAUTH_APIGW_PORT=8001"  \
-             -e "MEPAUTH_DB_SSLMODE=require" \
+             -e "MEPAUTH_DB_SSLMODE=verify-ca" \
              edgegallery/mepauth:latest
 
