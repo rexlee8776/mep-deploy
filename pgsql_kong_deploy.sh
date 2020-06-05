1aa2bc4d37d3c78a4aa4a23b8462bff74d9abdf9
Jing Lu <jing.s.lu@outlook.com>
2020-06-05 08:46:14 +0800

    Update pgsql_kong_deploy.sh
    Remove unnecessary port mapping for postgres.

--- pgsql_kong_deploy.sh
+++ pgsql_kong_deploy.sh
@@ -13,7 +13,6 @@ docker network create mep-net
 
 # run postgres db
 docker run -d --name postgres-db \
-                -p 5432:5432 \
                 --user=166:166 \
                 --network=mep-net \
                 -e "POSTGRES_USER=kong" \
@@ -54,7 +53,7 @@ docker run -d --name kong-service \
     -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
     -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
     -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
-    -e "KONG_ADMIN_LISTEN=0.0.0.0:8001, 0.0.0.0:8444 ssl" \
+    -e "KONG_ADMIN_LISTEN=0.0.0.0:8444 ssl" \
     -e "KONG_PG_SSL=on" \
     -e "KONG_PG_SSL_VERIFY=on" \
     -e "KONG_LUA_SSL_TRUSTED_CERTIFICATE=/run/kongssl/ca.crt" \
