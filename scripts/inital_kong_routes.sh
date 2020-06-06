a624ce1a2e2010723e1bced060d2b080fb5e405c
rexlee8776 <rexlee8776@gmail.com>
2020-06-06 14:12:28 +0800

    add domain name and cacert for kong curl setup
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

--- scripts/inital_kong_routes.sh
+++ scripts/inital_kong_routes.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-HOST=127.0.0.1
-KONG_ADMIN_PORT=8444
-MEPSERVER_PORT=30188
-
 set -x
 
+KONG_HOST=edgegallery.org
+KONG_ADMIN_PORT=8444
+MEPSERVER_HOST=127.0.0.1
+MEPSERVER_PORT=30188
+CACRT_PATH=/tmp/mepserver/ca.crt
+
+grep "${KONG_HOST}" /etc/hosts > /dev/null
+if [ $? -ne 0 ]; then
+    echo '127.0.0.1 edgegallery.org' >> /etc/hosts
+fi
+
+
 # create kong service for mepserver
-curl --location --request POST 'https://127.0.0.1:8444/services' \
+curl --cacert "${CACRT_PATH}" --location --request POST "https://${KONG_HOST}:${KONG_ADMIN_PORT}/services" \
 --header 'Content-Type: application/json' \
 --data-raw '{
-        "url": "https://127.0.0.1:30188",
+        "url": "https://'"${MEPSERVER_HOST}:${MEPSERVER_PORT}"'",
             "name": "https-mp1"
 }'
 
 # create kong route for mepserver
-curl --location --request POST 'https://127.0.0.1:8444/services/https-mp1/routes' \
+curl --cacert "${CACRT_PATH}" --location --request POST "https://${KONG_HOST}:${KONG_ADMIN_PORT}/services/https-mp1/routes" \
 --header 'Content-Type: application/json' \
 --data-raw '{
         "paths": ["/mepssl"],
@@ -23,7 +31,7 @@ curl --location --request POST 'https://127.0.0.1:8444/services/https-mp1/routes
 }'
 
 # enable jwt for mepserver service
-curl --location --request POST 'https://127.0.0.1:8444/services/https-mp1/plugins' \
+curl --cacert "${CACRT_PATH}" --location --request POST "https://${KONG_HOST}:${KONG_ADMIN_PORT}/services/https-mp1/plugins" \
 --header 'Content-Type: application/json' \
 --data-raw '{
         "name": "jwt"
