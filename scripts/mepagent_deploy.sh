a5640a9945e5284c47a8ab29d203db56117f860f
rexlee8776 <rexlee8776@gmail.com>
2020-06-07 11:50:08 +0800

    add mepagent deploy scripts
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

---
+++ scripts/mepagent_deploy.sh
@@ -0,0 +1,11 @@
+docker run –itd –name mepagent
+--network=mep-net
+-e MEP_IP=127.0.0.1
+-e MEP_AUTH_PORT=10443
+-e MEP_APIGW_PORT=8443
+-e ENABLE_WAIT=true
+-v /tmp/mepserver/ca.crt:/etc/mepssl/trust.cer
+-e "CA_CERT=/etc/mepssl/ca.crt"
+-e "CA_CERT_DOMAIN_NAME=edgegallery.org"
+-v app_instance_info.yaml:/usr/app/conf/app_instance_info.yaml \
+edgegallery/mep-agent:latest –ak QVUJMSUMgS0VZLS0tLS0 –sk DXPb4sqElKhcHe07Kw5uorayETwId1JOjjOIRomRs5wyszoCR5R7AtVa28KT3lSc
