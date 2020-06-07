bff6381cc21c322b2067d8339a73e1c54d339e85
Jing Lu <jing.s.lu@outlook.com>
2020-06-07 17:59:45 +0800

    Update pgsql_kong_deploy.sh
    Remove pg database port binding

--- scripts/pgsql_kong_deploy.sh
+++ scripts/pgsql_kong_deploy.sh
@@ -35,7 +35,6 @@ docker network create mep-net
 docker run -d --name postgres-db \
                 --user=166:166 \
                 --network=mep-net \
-                -p 5432:5432 \
                 -e "POSTGRES_USER=admin" \
                 -e "POSTGRES_DB=kong" \
                 -e "POSTGRES_PASSWORD=admin" \
