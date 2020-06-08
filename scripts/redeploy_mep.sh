cc69c055ddff7ae1a0738fc4cd2a2066483ae2d9
rexlee8776 <rexlee8776@gmail.com>
2020-06-08 11:58:03 +0800

    add pre deploy in redeploy
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

--- scripts/redeploy_mep.sh
+++ scripts/redeploy_mep.sh
@@ -7,12 +7,13 @@ docker rm -f kong-service
 docker rm -f postgres-db
 docker rm -f mepserver
 
+scripts/mep_pre_deploy.sh
+
 scripts/mepserver_deploy.sh
 
 scripts/pgsql_kong_deploy.sh
 
 sleep 5
-
 scripts/mepauth_deploy.sh -u mepauth -p mepauth -jwt te9Fmv%qaq
 
 scripts/check_env_status.sh
