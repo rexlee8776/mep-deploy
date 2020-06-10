7f669dde167aeb679c9031ae928883847f8faa8f
Jing Lu <jing.s.lu@outlook.com>
2020-06-10 08:32:39 +0800

    Update mepserver_deploy.sh

--- scripts/mepserver_deploy.sh
+++ scripts/mepserver_deploy.sh
@@ -6,7 +6,7 @@ CertDir=/tmp/${CertName}
 MepSslDir=/usr/mep/ssl
 
 # run mepserver docker
-docker run -itd --name mepserver --network mep-net -p 30188:8088 -e "SSL_ROOT=${MepSslDir}" \
+docker run -itd --name mepserver --network mep-net -e "SSL_ROOT=${MepSslDir}" \
                                  -v ${CertDir}/mepserver_tls.crt:${MepSslDir}/server.cer \
                                  -v ${CertDir}/mepserver_encryptedtls.key:${MepSslDir}/server_key.pem \
                                  -v ${CertDir}/ca.crt:${MepSslDir}/trust.cer \
