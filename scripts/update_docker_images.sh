c141310a801836e86bbc53b324fce59e536193f7
rexlee8776 <rexlee8776@gmail.com>
2020-06-10 14:24:31 +0800

    update images with clean old images
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

--- scripts/update_docker_images.sh
+++ scripts/update_docker_images.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 # update mep docker images
+docker rmi edgegallery/mep:latest
+docker rmi edgegallery/mepauth:latest
+docker rmi edgegallery/mep-agent:latest
+
+
 docker pull edgegallery/mep:latest
 docker pull edgegallery/mepauth:latest
 docker pull edgegallery/mep-agent:latest
