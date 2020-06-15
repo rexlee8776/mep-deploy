45d933766083932e351e3e8bb27d53dd9079b3a3
rexlee8776 <rexlee8776@gmail.com>
2020-06-15 10:53:58 +0800

    add openssl.cnf to generate v3 x509 certificates
    Isseu-ID:SEC-297
    
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

--- scripts/generate_cert.sh
+++ scripts/generate_cert.sh
@@ -5,12 +5,13 @@ set +o history
 set +x
 source scripts/mep_vars.sh
 
+cp scripts/openssl.cnf ${MEP_CERTS_DIR}/openssl.cnf
 cd ${MEP_CERTS_DIR}
 
 # generate ca certificate
 openssl genrsa -out ca.key 2048
 openssl req -new -key ca.key -subj /C=CN/ST=Peking/L=Beijing/O=edgegallery/CN=${DOMAIN_NAME} -out ca.csr
-openssl x509 -req -days 365 -in ca.csr -extensions v3_ca -signkey ca.key -out ca.crt
+openssl x509 -req -days 365 -in ca.csr -extfile openssl.cnf -extensions v3_ca -signkey ca.key -out ca.crt
 
 # generate tls certificate
 openssl genrsa -out ${CERT_NAME}_tls.key 2048
@@ -19,7 +20,7 @@ openssl rsa -in ${CERT_NAME}_tls.key -aes256 -passout pass:${AES_KEY_PW} -out ${
 echo -n ${AES_KEY_PW} > ${CERT_NAME}_cert_pwd
 
 openssl req -new -key ${CERT_NAME}_tls.key -subj /C=CN/ST=Beijing/L=Beijing/O=edgegallery/CN=${DOMAIN_NAME} -out ${CERT_NAME}_tls.csr
-openssl x509 -req -in ${CERT_NAME}_tls.csr -extensions v3_usr -CA ca.crt -CAkey ca.key -CAcreateserial -out ${CERT_NAME}_tls.crt
+openssl x509 -req -in ${CERT_NAME}_tls.csr -extfile openssl.cnf -extensions v3_req -CA ca.crt -CAkey ca.key -CAcreateserial -out ${CERT_NAME}_tls.crt
 
 # generate jwt public private key
 openssl genrsa -out jwt_privatekey 2048
