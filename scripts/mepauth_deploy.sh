25ff929bcf65196e74fe8c4065d77dccd0dc41fb
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2020-06-17 19:10:21 +0530

    username, password, ak, sk, app instance id are visible in MEP-Auth docker infromation.
    Issue-ID: SEC-331
    
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

--- scripts/mepauth_deploy.sh
+++ scripts/mepauth_deploy.sh
@@ -6,6 +6,10 @@ set +x
 source scripts/mep_vars.sh
 set -x
 
+cp -r conf /tmp/
+chown -R eguser:eggroup /tmp/conf/
+chmod -R 700 /tmp/conf/
+
 # deploy mepauth docker
 docker run -itd --name mepauth \
              --network mep-net \
@@ -16,11 +20,13 @@ docker run -itd --name mepauth \
              -v ${MEP_CERTS_DIR}/mepserver_tls.crt:${MEPAUTH_SSL_DIR}/server.crt:ro \
              -v ${MEP_CERTS_DIR}/mepserver_tls.key:${MEPAUTH_SSL_DIR}/server.key:ro \
              -v ${MEP_CERTS_DIR}/ca.crt:${MEPAUTH_SSL_DIR}/ca.crt:ro \
+             -v ${MEP_CONF_PATH}:/usr/mep/mepauth.properties \
              -e "MEPAUTH_DB_NAME=mepauth" \
              -e "MEPAUTH_DB_HOST=postgres-db" \
              -e "MEPAUTH_DB_PORT=5432" \
              -e "MEPAUTH_APIGW_HOST=kong-service" \
              -e "MEPAUTH_APIGW_PORT=8444"  \
              -e "MEPAUTH_CERT_DOMAIN_NAME=${DOMAIN_NAME}" \
-             edgegallery/mepauth:latest $*
+             edgegallery/mepauth:latest
+
 set -o history
