6768fae2d4d9fdf58a6e45588874a1492410bbea
Jing Lu <jing.s.lu@outlook.com>
2020-07-17 09:30:48 +0800

    Update mepauth_deploy.sh

--- scripts/mepauth_deploy.sh
+++ scripts/mepauth_deploy.sh
@@ -8,7 +8,7 @@ set -x
 
 cp -r mepauth-conf /tmp/
 chown -R eguser:eggroup /tmp/mepauth-conf/
-chmod -R 700 /tmp/mepauth-conf/
+chmod -R 600 /tmp/mepauth-conf/
 
 # deploy mepauth docker
 docker run -itd --name mepauth \
