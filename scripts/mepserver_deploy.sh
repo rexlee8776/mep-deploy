b8d16f6d7c5da15446edf2fcdd6d8a781e81641d
Jing Lu <jing.s.lu@outlook.com>
2020-07-17 17:21:07 +0800

    Update mepserver_deploy.sh

--- scripts/mepserver_deploy.sh
+++ scripts/mepserver_deploy.sh
@@ -8,6 +8,7 @@ set -x
 
 # run mepserver docker
 docker run -itd --name mepserver --network mep-net -e "SSL_ROOT=${MEPSERVER_SSL_DIR}" \
+                                 --cap-drop All \
                                  -v ${MEP_CERTS_DIR}/mepserver_tls.crt:${MEPSERVER_SSL_DIR}/server.cer:ro \
                                  -v ${MEP_CERTS_DIR}/mepserver_encryptedtls.key:${MEPSERVER_SSL_DIR}/server_key.pem:ro \
                                  -v ${MEP_CERTS_DIR}/ca.crt:${MEPSERVER_SSL_DIR}/trust.cer:ro \
