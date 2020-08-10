f36184ac88c503966231581213534b9d1b813ba1
rexlee8776 <rexlee8776@gmail.com>
2020-08-10 15:54:11 +0800

    need to start bin/start.sh after mepserver docker run up
    cert_pwd need to be input when start mepserver in docker
    
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

--- scripts/mepserver_deploy.sh
+++ scripts/mepserver_deploy.sh
@@ -7,12 +7,12 @@ source scripts/mep_vars.sh
 set -x
 
 # run mepserver docker
+## need to start bin/start.sh after mepserver docker run up
 docker run -itd --name mepserver --network mep-net -e "SSL_ROOT=${MEPSERVER_SSL_DIR}" \
                                  --cap-drop All \
                                  -v ${MEP_CERTS_DIR}/mepserver_tls.crt:${MEPSERVER_SSL_DIR}/server.cer:ro \
                                  -v ${MEP_CERTS_DIR}/mepserver_encryptedtls.key:${MEPSERVER_SSL_DIR}/server_key.pem:ro \
                                  -v ${MEP_CERTS_DIR}/ca.crt:${MEPSERVER_SSL_DIR}/trust.cer:ro \
-                                 -v ${MEP_CERTS_DIR}/mepserver_cert_pwd:${MEPSERVER_SSL_DIR}/cert_pwd:ro \
-                                 "$REGISTRY_URL"edgegallery/mep:latest
+                                 "$REGISTRY_URL"edgegallery/mep:latest sh
 
 set -o history
